6b4bc3306fa58d344657e4fe95b3a5adc61818ee
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-10-27 22:43:57 +0900

    emulatorのデータを保存するように変更、不要なファイルと記述削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { db } from "./service/firebase";
-import { getDoc, doc } from "@firebase/firestore";
-
-async function test() {
-  const userRef = doc(db, "users", "SXh73ZwkamCv3a3m0nrJ");
-  await getDoc(userRef)
-    .then((doc) => console.log(doc.data()))
-    .catch((error) => console.log(error));
-}
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={test}>Learn React</button>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
