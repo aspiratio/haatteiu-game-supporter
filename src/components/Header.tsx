363a9a29709d75dc1bf2fd2e9dd7900976511275
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-10-29 23:36:20 +0900

    ハンバーガーメニュー用プラグインを導入、ヘッダー作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="flex justify-center bg-yellow-300">
+      <h1 className="text-2xl font-medium p-1 text-white">HGS</h1>
+      <div className="tham tham-e-squeeze tham-w-6 absolute right-2 mt-3 mr-3">
+        <div className="tham-box tham-w-8">
+          <div className="tham-inner bg-yellow-800" />
+        </div>
+      </div>
+    </div>
+  );
+};
