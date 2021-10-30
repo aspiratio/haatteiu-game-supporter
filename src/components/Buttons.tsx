fd7dc7c3c590412032890d44e42992dd6c0007e8
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-10-30 23:35:25 +0900

    ボタンコンポーネントに6種類のボタンを作成

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,74 @@
+import { VFC } from "react";
+import "tailwindcss/colors";
+import { FaTwitter, FaQuestionCircle } from "react-icons/fa";
+
+type Props = {
+  text: string;
+};
+
+const StartButton: VFC<Props> = ({ text }) => {
+  return (
+    <button className="shadow-lg px-12 py-8 text-3xl rounded-3xl font-bold bg-yellow-300 text-white  hover:bg-yellow-400 hover:shadow-sm hover:translate-y-0.5 transform transition">
+      {text}
+    </button>
+  );
+};
+
+const PrimaryButton: VFC<Props> = ({ text }) => {
+  return (
+    <button className="px-6 py-2 text-xl rounded-lg border-2 border-blue-600 bg-blue-500 text-white hover:bg-blue-300">
+      {text}
+    </button>
+  );
+};
+
+const SecondButton: VFC<Props> = ({ text }) => {
+  return (
+    <button className="px-6 py-2 text-xl rounded-lg border-2 bg-blue-100 border-blue-500 text-blue-500 hover:text-white hover:bg-blue-400">
+      {text}
+    </button>
+  );
+};
+
+const ThirdButton: VFC<Props> = ({ text }) => {
+  return (
+    <button className="px-3 py-2 text-xl rounded-lg border-2 bg-gray-200 border-gray-500 text-gray-600 hover:text-white hover:bg-gray-500">
+      {text}
+    </button>
+  );
+};
+
+const TwitterButton: VFC = () => {
+  return (
+    <>
+      <button className="inline-flex rounded-md shadow-lg bg-blue-400 hover:bg-blue-300">
+        <FaTwitter className="h-auto w-auto p-2 rounded-md text-white" />
+        <span className="text-sm pr-2 pl-0 my-auto rounded-md text-white">
+          ツイート
+        </span>
+      </button>
+    </>
+  );
+};
+
+const HowToUseButton: VFC = () => {
+  return (
+    <>
+      <button className="inline-flex rounded-md shadow-lg bg-gray-300 hover:bg-gray-400">
+        <FaQuestionCircle className="h-auto w-auto p-2 rounded-md" />
+        <span className="text-sm pr-2 pl-0 my-auto rounded-md text-gray-500">
+          使い方
+        </span>
+      </button>
+    </>
+  );
+};
+
+export {
+  StartButton,
+  PrimaryButton,
+  SecondButton,
+  ThirdButton,
+  TwitterButton,
+  HowToUseButton,
+};
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,7 +1,32 @@
+import {
+  HowToUseButton,
+  PrimaryButton,
+  SecondButton,
+  StartButton,
+  ThirdButton,
+  TwitterButton,
+} from "../components/Buttons";
+
 export const Top = () => {
   return (
     <>
       <h1>Topページです</h1>
+      <StartButton text={"ゲームを始める"} />
+      <br />
+      <br />
+      <PrimaryButton text={"ゲーム開始"} />
+      <br />
+      <br />
+      <SecondButton text={"中止"} />
+      <br />
+      <br />
+      <ThirdButton text={"コピー"} />
+      <br />
+      <br />
+      <TwitterButton />
+      <br />
+      <br />
+      <HowToUseButton />
     </>
   );
 };
