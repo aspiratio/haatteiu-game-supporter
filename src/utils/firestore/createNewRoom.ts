cf84f8bf1d3bd2563e2bf9a0d1630565e016badd
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-11-18 22:45:26 +0900

    [clean]変数名の変更

--- src/utils/firestore/createNewRoom.ts
+++ src/utils/firestore/createNewRoom.ts
@@ -2,9 +2,9 @@ import { collection, doc, serverTimestamp, setDoc } from "@firebase/firestore";
 import { db } from "../../service/firebase";
 
 export const createNewRoom = async (name: string) => {
-  const newRoomRef = doc(collection(db, "hgs/v1/rooms"));
-  const usersRef = doc(collection(db, `hgs/v1/rooms/${newRoomRef.id}/users`));
-  await setDoc(newRoomRef, { createdAt: serverTimestamp(), usersName: [name] });
+  const roomsRef = doc(collection(db, "hgs/v1/rooms"));
+  const usersRef = doc(collection(db, `hgs/v1/rooms/${roomsRef.id}/users`));
+  await setDoc(roomsRef, { createdAt: serverTimestamp(), usersName: [name] });
   await setDoc(usersRef, { displayName: name, isHost: true });
-  return newRoomRef.id;
+  return roomsRef.id;
 };
