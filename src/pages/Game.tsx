319c3c3d539918a38198fd9747e22081332b6661
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-12-04 11:59:14 +0900

    [add]ユーザー情報の取得と表示するように

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,51 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GameTabs } from "../components/GameTabs";
 import { Information } from "../components/Information";
 import { ThemeContent } from "../components/contents/ThemeContent";
 import { AnswersContent } from "../components/contents/AnswersContent";
 import { PointsContent } from "../components/contents/PointsContent";
 import { getObjFromSessionStorage } from "../utils/getObjFromSessionStorage";
-import { collection, doc, getDoc, getDocs } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+} from "@firebase/firestore";
 import { db } from "../service/firebase";
 
-export const Game = () => {
-  const { userName, roomId, userId } = getObjFromSessionStorage("userInfo");
-  // TODO:firestoreとのやりとりを隠匿する
-  const fetchRoom = async () => {
-    const roomRef = doc(db, `hgs/v1/rooms/${roomId}`);
-    const roomSnapshot = await getDoc(roomRef);
-    return roomSnapshot.data();
-  };
-  const roomData = fetchRoom();
+type Type = {
+  userName: string;
+  roomId: string;
+  userId: string;
+};
 
-  const fetchAllUsers = async () => {
-    const usersCollection = collection(db, `hgs/v1/rooms/${roomId}/users`);
-    const usersSnapshot = await getDocs(usersCollection);
-    return usersSnapshot.docs.map((doc) => {
-      return doc.data();
-    });
-  };
-  console.log(fetchAllUsers());
+export const Game = () => {
+  const { userName, roomId, userId }: Type =
+    getObjFromSessionStorage("userInfo");
+  const [usersName, setUsersName] = useState<Array<string>>([]);
+  const [userAlphabet, setUserAlphabet] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const fetchRoom = async () => {
+        const roomRef = doc(db, `hgs/v1/rooms/${roomId}`);
+        const roomSnapshot = await getDoc(roomRef);
+        return roomSnapshot.data();
+      };
+      const fetchUser = async () => {
+        const userRef = doc(db, `hgs/v1/rooms/${roomId}/users/${userId}`);
+        const userSnapshot = await getDoc(userRef);
+        return userSnapshot.data();
+      };
+      const orderByAllUsers = async () => {
+        const usersQuery = query(
+          collection(db, `hgs/v1/rooms/${roomId}/users`),
+          orderBy("actOrder")
+        );
+        const usersSnapshot = await getDocs(usersQuery);
+        return usersSnapshot.docs.map((doc) => {
+          return doc.data();
+        });
+      };
+
+      const roomData = await fetchRoom();
+      const correctAnswer = roomData!.correctAnswer as string[];
+      const userData = await fetchUser();
+      const actOrder = userData!.actOrder;
+      setUserAlphabet(correctAnswer[actOrder]);
+
+      const allUsersData = await orderByAllUsers();
+      const allUsersName = allUsersData.map((data) => {
+        return data.displayName as string;
+      });
+      setUsersName(allUsersName);
+    })();
+  }, []);
+  // TODO:firestoreとのやりとりを隠匿する
+
   // const userRef = doc(db, `hgs/v1/rooms/${roomId}/users/${userId}`);
   // const userSnapshot = await getDoc(userRef);
   // const userData = userSnapshot.data();
 
   const [actorNumber, setActorNumber] = useState(0);
   // firestoreから取得（演技順をfirestoreに登録する必要あり？）
-  const usersName = ["a"];
-  const userAlphabet = "a";
 
   const isFinished = actorNumber === usersName.length;
-
-  // (async () => {
-  //   const usersRef = collection(db, `hgs/v1/rooms/${roomId}/users`);
-  //   const usersDoc = await getDocs(usersRef);
-  //   const usersOrder = new Array(usersDoc.size)
-  //   usersDoc.forEach((userDoc) => {
-  //     const actOrder = userDoc.data().actOrder as number
-  //     usersOrder[actOrder] =
-  //   })
-  // })();
 
   type Tab = "theme" | "answers" | "points";
   const [activeTab, setActiveTab] = useState<Tab>("theme");
